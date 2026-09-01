8e00e7604d0f4f8738e961dd00f2b7e8be90c5b5
MOULEESWARAN P <strangedr965@gmail.com>
2026-09-01 16:11:51 +0530

    [Array Methods & ES6 Transpilation] Learn array methods (.includes()), ES6 features, and Babel transpilation

---
+++ Day-2/output.js
@@ -0,0 +1,7 @@
+"use strict";
+
+require("core-js/modules/es.array.includes.js");
+var nums = [1, 2, 3];
+console.log(nums.includes(2));
+var name = 'Moulee';
+console.log("Hello ".concat(name));
---
+++ Day-2/sample.js
@@ -0,0 +1,4 @@
+const nums = [1, 2, 3];
+console.log(nums.includes(2));
+const name = 'Moulee';
+console.log(`Hello ${name}`);
